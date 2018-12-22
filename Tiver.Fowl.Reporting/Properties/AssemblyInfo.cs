72c283350243bed42f5205b5d0c8397399218672
Artsiomi Silivonchyk <mr.hant@gmail.com>
2018-12-22 09:59:27 +0300

    Add initial solution files

---
+++ Tiver.Fowl.Reporting/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C1F39C10-F7D9-4098-A620-813824B2C4CE")]
+[assembly: AssemblyVersion("1.0.*")]
