972104b64a45a9f97c89b38c3e7bf9f454252600
Artsiomi Silivonchyk <mr.hant@gmail.com>
2018-12-24 12:52:41 +0300

    Add Report class

---
+++ Tiver.Fowl.Reporting.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("24C1FD8B-3A9D-4C78-AE55-9D6D9F140870")]
+[assembly: AssemblyVersion("1.0.*")]
